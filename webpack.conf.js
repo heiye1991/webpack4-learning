501e562f9452028a3e95053614878f81cca28c37
heiye1991 <1604043458@qq.com>
2018-11-05 10:31:00 +0800

    删除无用的css样式

--- webpack.conf.js
+++ webpack.conf.js
@@ -5,6 +5,8 @@
  */
 const path = require('path')
 const ExtractTextPlugin = require('extract-text-webpack-plugin'); //引入css分离插件
+const PurifyCss = require('purifycss-webpack'); // 引入PurifyCssWebpack插件
+const glob = require('glob');  // 引入glob模块,用于扫描全部html文件中所引用的css
 module.exports = {
   mode: 'production',
   entry: {
@@ -50,8 +52,11 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: 'bundle.css', //生成文件的文件名。可能包含 [name], [id] and [contenthash]
-      allChunks: false // 只包括初始化css, 不包括异步加载的CSS---从所有额外的 chunk(additional chunk) 提取（默认情况下false，它仅从初始chunk(initial chunk) 中提取）当使用 CommonsChunkPlugin 并且在公共 chunk 中有提取的 chunk（来自ExtractTextPlugin.extract）时，allChunks **必须设置为 true
+      filename: '[name].min.css', //生成文件的文件名。可能包含 [name], [id] and [contenthash]
+      // allChunks: false // 只包括初始化css, 不包括异步加载的CSS---从所有额外的 chunk(additional chunk) 提取（默认情况下false，它仅从初始chunk(initial chunk) 中提取）当使用 CommonsChunkPlugin 并且在公共 chunk 中有提取的 chunk（来自ExtractTextPlugin.extract）时，allChunks **必须设置为 true
+    }),
+    new PurifyCss({
+      paths: glob.sync(path.join(__dirname, './*.html')) // 同步扫描所有html文件中所引用的css
     })
     // new webpack.optimize.UglifyJsPlugin() //已被移除 用optimization.minimize代替
   ],
