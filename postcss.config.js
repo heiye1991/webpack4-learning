5670170e853ec4c5c52df8b4f01f79c5b0e7fc44
heiye1991 <1604043458@qq.com>
2018-11-06 15:09:02 +0800

    优化代码以及webpack 搭建本地开发环境

--- postcss.config.js
+++ postcss.config.js
@@ -4,23 +4,9 @@
  * description:
  */
 module.exports = {
-  // 可以使用数组写法，也可以使用对象写法
-  plugins: {
-    // 使用postcss-sprites报错，版本要求postcss5.2.18，但是项目使用了高版本7.0.5
-    /*'postcss-sprites': {
-      spritePath: "./dist/images/sprites",//雪碧图合成路径
-      retina: true // retina屏幕 还需要把需要处理的对应图片改为@2x等
-    },*/
-    // 'autoprefixer': {}
-    'postcss-cssnext': {}, // 未来的css属性，使用这个就不需要autoprefixer了
-    'cssnano': {}, //cssnano是PostCSS的CSS优化和分解插件。cssnano采用格式很好的CSS，并通过许多优化，以确保最终的生产环境尽可能小
-  }
-  /*plugins: [
-    require('autoprefixer'),
+  plugins: [
+    // require('autoprefixer'),
     require('postcss-cssnext'),
-    require('cssnano'),
-    require('postcss-sprites')({
-      spritePath: "./dist/images/sprites"
-    })
-  ]*/
+    require('cssnano')
+  ]
 }
--- webpack.conf.js
+++ webpack.conf.js
@@ -2,38 +2,46 @@
  * created by : heiye1991
  * created time: 2018-11-02
  * description:
- *    打包html ----html-webpack-plugin
- *    处理html里的图片问题方案1 ---html-loader，可以配置处理src和data-src等属性
- *    处理html里的图片问题方案2 ---html-withimg-loader，不能处理data-src
- *    处理html里的图片问题方案3 ---html里面的图片地址使用${require(路径)}，这样不用使用上面的loader
- *
+ *    搭建开发环境
+ *    clean-webpack-plugin自动清理打包已生成的dist，避免冗余文件存在
+ *    webpack -w 或者webpack -watch 本地监听服务变化自动打包
+ *    webpack-dev-server 官方推荐的开发服务器，可以集成多种功能
+ *    express+webpack-dev-middleware 第三方搭建开发环境，可以配置自己想要的服务
  */
 const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin'); //引入css分离插件
 const PurifyCss = require('purifycss-webpack'); // 引入PurifyCssWebpack插件
 const glob = require('glob');  // 引入glob模块,用于扫描全部html文件中所引用的css
-// const SpritesmithPlugin = require('webpack-spritesmith') //雪碧图合成处理位图资源
 const HtmlWebpackPlugin = require('html-webpack-plugin'); // 引入HtmlWebpackPlugin插件
+const CleanWebpackPlugin = require('clean-webpack-plugin'); // 引入CleanWebpackPlugin插件
 module.exports = {
   mode: 'production',
   entry: {
     index: path.join(__dirname, './src/index.js')
   },
   output: {
-    // publicPath: './dist/', // 不打包生成HTML时
     publicPath: './', // 打包生成HTML时
     path: path.resolve(__dirname, './dist'), // 打包文件的输出目录
-    filename: '[name].bundle-[hash:5].js' // 打包生成的文件
+    filename: 'js/[name].bundle-[hash:5].js' // 打包生成的文件
   },
   module: {
     rules: [
+      // js 打包
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader"
+        },
+        exclude: /node_modules/
+      },
       // css打包
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
           fallback: 'style-loader',
-          use: ['css-loader', 'postcss-loader']
+          use: ['css-loader', 'postcss-loader'],
+          publicPath: '../'  // 给背景图片设置一个公共路径
         })
       },
       // less打包
@@ -41,7 +49,8 @@ module.exports = {
         test: /\.less$/,
         use: ExtractTextPlugin.extract({
           fallback:"style-loader",
-          use:['css-loader', 'postcss-loader', 'less-loader']
+          use:['css-loader', 'postcss-loader', 'less-loader'],
+          publicPath: '../'  // 给背景图片设置一个公共路径
         })
       },
       // sass/scss 打包
@@ -49,7 +58,8 @@ module.exports = {
         test: /\.(scss|sass)$/,
         use: ExtractTextPlugin.extract({
           fallback:"style-loader",
-          use:['css-loader', 'postcss-loader', 'sass-loader']
+          use:['css-loader', 'postcss-loader', 'sass-loader'],
+          publicPath: '../'  // 给背景图片设置一个公共路径
         })
       },
       // stylus打包
@@ -57,22 +67,14 @@ module.exports = {
         test: /\.styl$/,
         use: ExtractTextPlugin.extract({
           fallback:'style-loader',
-          use:['css-loader', 'postcss-loader', 'stylus-loader']
+          use:['css-loader', 'postcss-loader', 'stylus-loader'],
+          publicPath: '../'  // 给背景图片设置一个公共路径
         })
       },
       // 图片打包
       {
-        test: /\.(png|jpg|svg|gif)$/,  // 正则匹配图片格式名
+        test: /\.(png|jpg|gif)$/,  // 正则匹配图片格式名 没有用到svg图片|svg去掉
         use: [
-          // file-loader打包图片
-          /*{
-            loader: 'file-loader',
-            options: {
-              outputPath: 'images',  // 设置打包后图片存放的文件夹名称
-              name: "[name]-[hash:5].min.[ext]",
-              publicPath: './images'  // 给图片设置一个公共路径
-            }
-          }*/
           // url-loader 打包图片，可以设置图片使用base64
           {
             loader: 'url-loader',
@@ -80,7 +82,6 @@ module.exports = {
               limit: 20000,  // 限制只有小于20kb的图片才转为base64，超过这个大小不会被转化
               outputPath: 'images',  // 设置打包后图片存放的文件夹名称
               name: "[name]-[hash:5].min.[ext]",// 打包后的图片命名
-              publicPath: './images',  // 给图片设置一个公共路径
               fallback:'file-loader' //大于limit限制的将转交给指定的loader处理
             }
           },
@@ -89,11 +90,6 @@ module.exports = {
             loader: 'img-loader',
             options: {
               plugins: [
-                // 处理gif
-                require('imagemin-gifsicle')({
-                  interlaced: false,
-                  optimizationLevel: 3
-                }),
                 // 处理jpg
                 require('imagemin-mozjpeg')({
                   progressive: true,
@@ -104,40 +100,23 @@ module.exports = {
                   floyd: 0.5,
                   speed: 2,
                   quality: 80
-                }),
-                // 处理svg
-                require('imagemin-svgo')({
-                  plugins: [
-                    { removeTitle: true },// 去掉 <title>
-                    { convertPathData: true }, //将路径数据转换为相对或绝对（以较短者为准），将一个段转换为另一个段，修剪无用的分隔符，智能舍入等等
-                    { removeDesc: true }, //去掉 <desc>
-                    { minifyStyles: true }, //<style>用CSSO缩小元素内容
-                    { removeEmptyText: true }, //删除空文本元素
-                    { removeEmptyAttrs: true }, //删除空属性
-                  ]
                 })
               ]
             }
           }
         ]
       },
-      /*
-      // svg 可以使用inline-svg-loader进行资源嵌入，也可以使用svg-sprite-loader将矢量图资源合并为雪碧图
-      {
-        test: /\.svg$/,
-        use: 'inline-svg-loader'
-      },*/
       // 字体打包
       {
-        test: /\.(woff2?|eot|ttf)(\?.*)?$/,
+        test: /\.(woff2?|eot|ttf|svg)(\?.*)?$/,
         use: [
           {
             loader: 'url-loader',
             options: {
               limit: 100,
               name: '[name].[hash:7].[ext]',
-              outputPath: 'fonts',
-              publicPath: './fonts'
+              outputPath: 'fonts', // 设置打包后字体存放的文件夹名称
+              publicPath: '../fonts' // 给字体设置一个公共路径
             }
           }
         ]
@@ -153,76 +132,33 @@ module.exports = {
             }
           }
         ]
-        // use: ['html-withimg-loader']
       }
-      // imports-loader处理第三方js库
-      /*{
-        test: path.resolve(__dirname, 'src/index.js'),
-        use: [
-          {
-            loader: 'imports-loader',
-            options: {
-              $: 'jquery',
-              jQuery: 'jquery'
-            }
-          }
-        ]
-      }*/
     ]
-  },
-  resolve: {
-    alias: {
-      // 名称和new webpack.ProvidePlugin里的jQuery对应，加$是只把jQuery关键字解析到某个文件下，而不是目录
-      jQuery$: path.resolve(__dirname, "./src/libs/jquery.js")
-    }
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: '[name]-[hash:5].min.css', //生成文件的文件名。可能包含 [name], [id] and [contenthash]
+      filename: 'css/[name]-[hash:5].min.css', //生成文件的文件名。可能包含 [name], [id] and [contenthash]
       // allChunks: false // 只包括初始化css, 不包括异步加载的CSS---从所有额外的 chunk(additional chunk) 提取（默认情况下false，它仅从初始chunk(initial chunk) 中提取）当使用 CommonsChunkPlugin 并且在公共 chunk 中有提取的 chunk（来自ExtractTextPlugin.extract）时，allChunks **必须设置为 true
     }),
-    new PurifyCss({
-      paths: glob.sync(path.join(__dirname, './*.html')) // 同步扫描所有html文件中所引用的css
-    }),
+    // new PurifyCss({
+    //   paths: glob.sync(path.join(__dirname, './*.html')) // 同步扫描所有html文件中所引用的css, 会删除无用的css
+    // }),
     // 使用第三方js库
     new webpack.ProvidePlugin({
-      $: 'jquery', // npm
-      jQuery: 'jQuery' // 本地Js文件
+      $: 'jquery',
+      jQuery: 'jquery'
     }),
     new HtmlWebpackPlugin({
       template: path.join(__dirname, "./index.html"),// new一个这个插件的实例，并传入相关的参数
       filename: 'index.html',
-      // inject: 'body', //默认注入到body里面，可以设为head或者body，false不注入
       minify: {
         removeComments: true, //删除注释
         collapseWhitespace: true // 去除空格
       },
       // chunks: [], // 指定打包生成的HTML引用的js
       // excludeChunks: [] //排除那些js不打包到生成的js
-    })
-    // 生成雪碧图，在这里没有实现。。。。。。
-    /*new SpritesmithPlugin({
-      //设置源icons,即icon的路径，必选项
-      src: {
-        cwd: __dirname + './src/images/',
-        glob: '*.png' //正则匹配，照着填即可
-      },
-      //设置导出的sprite图及对应的样式文件，必选项
-      target: {
-        image: __dirname + './dist/images/sprites/sprite.png',
-        css: __dirname + './dist/images/sprites/sprite.css'
-      },
-      //设置sprite.png的引用格式，会自己加入sprite.css的头部
-      apiOptions: {
-        cssImageRef: './sprite.png' //cssImageRef为必选项
-      },
-      //配置spritesmith选项，非必选
-      spritesmithOptions: {
-        algorithm: 'top-down',//设置图标的排列方式
-        padding: 4 //每张小图的补白,避免雪碧图中边界部分的bug
-      }
-    })*/
-    // new webpack.optimize.UglifyJsPlugin() //已被移除 用optimization.minimize代替
+    }),
+    new CleanWebpackPlugin(['dist']),  // 所要清理的文件夹名称
   ],
   optimization: {
     minimize: true // 默认false不压缩js，true压缩js
