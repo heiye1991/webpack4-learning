cbe5baff22a5ea16e6955cdbc99b26fa8697f27d
heiye1991 <1604043458@qq.com>
2018-11-06 11:02:29 +0800

    打包html以及图片标签引用图片处理

--- webpack.conf.js
+++ webpack.conf.js
@@ -2,29 +2,33 @@
  * created by : heiye1991
  * created time: 2018-11-02
  * description:
- *    字体图标打包
- *    匹配字体图标和匹配图片里的svg全都打包到fonts文件夹，未解决。。。。。。
- *    临时方案，取消字体图标打包里面的svg匹配
+ *    打包html ----html-webpack-plugin
+ *    处理html里的图片问题方案1 ---html-loader，可以配置处理src和data-src等属性
+ *    处理html里的图片问题方案2 ---html-withimg-loader，不能处理data-src
+ *    处理html里的图片问题方案3 ---html里面的图片地址使用${require(路径)}，这样不用使用上面的loader
+ *
  */
 const path = require('path')
 const webpack = require('webpack')
 const ExtractTextPlugin = require('extract-text-webpack-plugin'); //引入css分离插件
 const PurifyCss = require('purifycss-webpack'); // 引入PurifyCssWebpack插件
 const glob = require('glob');  // 引入glob模块,用于扫描全部html文件中所引用的css
-const SpritesmithPlugin = require('webpack-spritesmith') //雪碧图合成处理位图资源
+// const SpritesmithPlugin = require('webpack-spritesmith') //雪碧图合成处理位图资源
+const HtmlWebpackPlugin = require('html-webpack-plugin'); // 引入HtmlWebpackPlugin插件
 module.exports = {
   mode: 'production',
   entry: {
     index: path.join(__dirname, './src/index.js')
   },
   output: {
-    publicPath: './dist/',
+    // publicPath: './dist/', // 不打包生成HTML时
+    publicPath: './', // 打包生成HTML时
     path: path.resolve(__dirname, './dist'), // 打包文件的输出目录
-    filename: '[name].bundle.js', // 打包生成的文件
-    chunkFilename: "[name].chunk.js"
+    filename: '[name].bundle-[hash:5].js' // 打包生成的文件
   },
   module: {
     rules: [
+      // css打包
       {
         test: /\.css$/,
         use: ExtractTextPlugin.extract({
@@ -32,6 +36,7 @@ module.exports = {
           use: ['css-loader', 'postcss-loader']
         })
       },
+      // less打包
       {
         test: /\.less$/,
         use: ExtractTextPlugin.extract({
@@ -39,6 +44,7 @@ module.exports = {
           use:['css-loader', 'postcss-loader', 'less-loader']
         })
       },
+      // sass/scss 打包
       {
         test: /\.(scss|sass)$/,
         use: ExtractTextPlugin.extract({
@@ -46,6 +52,7 @@ module.exports = {
           use:['css-loader', 'postcss-loader', 'sass-loader']
         })
       },
+      // stylus打包
       {
         test: /\.styl$/,
         use: ExtractTextPlugin.extract({
@@ -53,6 +60,7 @@ module.exports = {
           use:['css-loader', 'postcss-loader', 'stylus-loader']
         })
       },
+      // 图片打包
       {
         test: /\.(png|jpg|svg|gif)$/,  // 正则匹配图片格式名
         use: [
@@ -119,7 +127,7 @@ module.exports = {
         test: /\.svg$/,
         use: 'inline-svg-loader'
       },*/
-      // 字体处理
+      // 字体打包
       {
         test: /\.(woff2?|eot|ttf)(\?.*)?$/,
         use: [
@@ -134,8 +142,21 @@ module.exports = {
           }
         ]
       },
-      // imports-loader处理第三方js库
+      // HTML里的图片打包
       {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: {
+              attrs: ['img:src', 'img:data-src']
+            }
+          }
+        ]
+        // use: ['html-withimg-loader']
+      }
+      // imports-loader处理第三方js库
+      /*{
         test: path.resolve(__dirname, 'src/index.js'),
         use: [
           {
@@ -146,7 +167,7 @@ module.exports = {
             }
           }
         ]
-      }
+      }*/
     ]
   },
   resolve: {
@@ -157,17 +178,28 @@ module.exports = {
   },
   plugins: [
     new ExtractTextPlugin({
-      filename: '[name].min.css', //生成文件的文件名。可能包含 [name], [id] and [contenthash]
+      filename: '[name]-[hash:5].min.css', //生成文件的文件名。可能包含 [name], [id] and [contenthash]
       // allChunks: false // 只包括初始化css, 不包括异步加载的CSS---从所有额外的 chunk(additional chunk) 提取（默认情况下false，它仅从初始chunk(initial chunk) 中提取）当使用 CommonsChunkPlugin 并且在公共 chunk 中有提取的 chunk（来自ExtractTextPlugin.extract）时，allChunks **必须设置为 true
     }),
     new PurifyCss({
       paths: glob.sync(path.join(__dirname, './*.html')) // 同步扫描所有html文件中所引用的css
     }),
     // 使用第三方js库
-    /*new webpack.ProvidePlugin({
+    new webpack.ProvidePlugin({
       $: 'jquery', // npm
       jQuery: 'jQuery' // 本地Js文件
-    })*/
+    }),
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, "./index.html"),// new一个这个插件的实例，并传入相关的参数
+      filename: 'index.html',
+      // inject: 'body', //默认注入到body里面，可以设为head或者body，false不注入
+      minify: {
+        removeComments: true, //删除注释
+        collapseWhitespace: true // 去除空格
+      },
+      // chunks: [], // 指定打包生成的HTML引用的js
+      // excludeChunks: [] //排除那些js不打包到生成的js
+    })
     // 生成雪碧图，在这里没有实现。。。。。。
     /*new SpritesmithPlugin({
       //设置源icons,即icon的路径，必选项
